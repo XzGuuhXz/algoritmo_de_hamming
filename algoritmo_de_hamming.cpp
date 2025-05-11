edd3291b997ee2d54af3b9f9888b11a742f68bf7
gustavo <gustavohenrique07101427@gmail.com>
2025-05-11 17:33:55 -0400

    update

--- algoritmo_de_hamming.cpp
+++ algoritmo_de_hamming.cpp
@@ -4,103 +4,105 @@ using namespace std ;
 
 class Hamming
 {
-    private:
+  private:
          
 
-    public:
-        int decodificador();
-        int codificador();
-        void menu();
+  public:
+    int decodificador();
+    int codificador();
+    void menu();
 };
 
 int Hamming::decodificador()
 {
-    int quantidade;
-    cout << "Digite a quantidade de bits que deseja: ";
-    cin >> quantidade;
+  int quantidade;
 
-    int *bit = new int[quantidade];
+  cout << "Digite a quantidade de bits que deseja: ";
+  cin >> quantidade;
 
-    cout << "Digite os bits (0 ou 1):\n";
-    for (int i = 0; i < quantidade; i++) 
+  int *bit = new int[quantidade];
+
+  cout << "Digite os bits (0 ou 1):\n";
+  for (int i = 0; i < quantidade; i++) 
+  {
+    cout << "Bit " << i << ": ";
+    cin >> bit[i];
+
+    while (bit[i] != 0 && bit[i] != 1) 
     {
-        cout << "Bit " << i << ": ";
-        cin >> bit[i];
+      cout << "Bit inválido! Digite 0 ou 1: ";
+      cin >> bit[i];
+    }
+  }
 
-        while (bit[i] != 0 && bit[i] != 1) 
-        {
-            cout << "Bit inválido! Digite 0 ou 1: ";
-            cin >> bit[i];
-        }
-    }
+  int paridade = 0;
 
-    int paridade = 0;
-    for(int i = 0; i < quantidade; i++)
+  for(int i = 0; i < quantidade; i++)
+  {
+    if(bit[i]== 1)
     {
-        if(bit[i]== 1)
-        {
-            paridade ^= (i + 1);
-        }
+      paridade ^= (i + 1);
     }
+  }
 
-    if(paridade == 0)
-    {
-        cout << "Nem um erro foi encontrado!!" << "\n\n" << endl;
-    }
-    else
-    {
-        cout << "Erro encontado no bit de posição: " << paridade << endl;
-    }
+  if(paridade == 0)
+  {
+    cout << "Nem um erro foi encontrado!!" << "\n\n" << endl;
+  }
+  else
+  {
+    cout << "Erro encontado no bit de posição: " << paridade << endl;
+  }
     
-    delete[] bit;
-    return paridade;
+  delete[] bit;
+  return paridade;
 }
 
 int Hamming::codificador()
 {
-    return 0;
+  return 0;
 }
 
 int main()
 {
-    Hamming *root = new Hamming();
+  Hamming *root = new Hamming();
 
-    root->menu();
+  root->menu();
 
-    delete root;
+  delete root;
 
-    return 0;
+  return 0;
 
 }
 
 void Hamming::menu()
 {
-    int opc;
+  int opc;
 
-    do 
+  do 
+  {
+    std::cout << "\tMenu\n";
+    std::cout << "\t1 - Decodificador\n";
+    std::cout << "\t2 - Codificador\n";
+    std::cout << "\t0 - Sair\n";
+    std::cout << "\tEscolha uma opção: ";
+    std::cin >> opc;
+
+    switch(opc)
     {
-        std::cout << "\tMenu\n";
-        std::cout << "\t1 - Decodificador\n";
-        std::cout << "\t2 - Codificador\n";
-        std::cout << "\t0 - Sair\n";
-        std::cout << "\tEscolha uma opção: ";
-        std::cin >> opc;
+      case 1:
+        decodificador();
+        break;
+      case 2:
+        codificador();
+        break;
+      case 0:
+        std::cout << "Saindo...\n";
+        break;
+      default:
+        std::cout << "Opção inválida.\n";
+        break;
+    }
 
-        switch(opc)
-        {
-            case 1:
-                decodificador();
-                break;
-            case 2:
-                codificador();
-                break;
-            case 0:
-                std::cout << "Saindo...\n";
-                break;
-            default:
-                std::cout << "Opção inválida.\n";
-                break;
-        }
-
-    } while(opc != 0);
+  } while(opc != 0);
 }
